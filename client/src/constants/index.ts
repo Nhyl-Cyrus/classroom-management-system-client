40adec8ccbd3944c663e10bf04fde35934aa244a
Nhyl <nhylcyrusgervasio@gmail.com>
2026-06-13 02:48:04 +0800

    convert client from submodule to regular directory

---
+++ client/src/constants/index.ts
@@ -0,0 +1,12 @@
+export const DEPARTMENTS = [
+    "Computer Science",
+    "Mathematics",
+    "Physics",
+    "Chemistry",
+    "Biology",
+]
+
+export const DEPARTMENT_OPTIONS = DEPARTMENTS.map((dept) => ({
+    value: dept,
+    label: dept,
+}))
---
+++ client/src/providers/data.ts
@@ -0,0 +1,35 @@
+import { MOCK_SUBJECTS } from "@/constants/mock-data";
+import {
+  BaseRecord,
+  DataProvider,
+  GetListParams,
+  GetListResponse,
+} from "@refinedev/core";
+
+export const dataProvider: DataProvider = {
+  getList: async <TData extends BaseRecord = BaseRecord>({
+    resource,
+  }: GetListParams): Promise<GetListResponse<TData>> => {
+    if (resource !== "subjects") return { data: [] as TData[], total: 0 };
+
+    return {
+      data: MOCK_SUBJECTS as unknown as TData[],
+      total: MOCK_SUBJECTS.length,
+    };
+  },
+
+  getOne: async () => {
+    throw new Error("Method not implemented.");
+  },
+  create: async () => {
+    throw new Error("Method not implemented.");
+  },
+  update: async () => {
+    throw new Error("Method not implemented.");
+  },
+  deleteOne: async () => {
+    throw new Error("Method not implemented.");
+  },
+
+  getApiUrl: () => "",
+};
